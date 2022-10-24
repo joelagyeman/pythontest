7c4feb3e9ba54e8701cf45caf18ed7305980b567
Joel Bill Agyeman-Mensah <joelagyeman@meltwater.org>
2022-10-24 06:31:39 +0000

    second commit

---
+++ even_numbers.py
@@ -0,0 +1,17 @@
+#for loop
+
+number, end = 1, 30
+
+
+for num in range(number, end + 1):
+   
+    if num % 2 == 0:
+        print(num, end=" ")
+
+#while loop
+
+i = 1
+while i <= 30:
+    if i %2 == 0:
+        print(i)
+    i = i+1
---
+++ odd_numbers.py
@@ -0,0 +1,15 @@
+#For loop
+start, end = 1, 30 
+ 
+
+for num in range(start, end + 1):
+     
+    if num % 2 != 0:
+        print(num, end = " ") 
+
+#while loop
+i = 1
+while i <= 30:
+    if i % 2 !=0:
+        print(i)
+    i = i+1
